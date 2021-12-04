1bb68d191bff09f47fe6d58c92774ecd14ba4d96
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-12-04 03:11:13 -0500

    creando marcador con color v2

--- appmaps/lib/main.dart
+++ appmaps/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'src/locations.dart' as locations;
@@ -14,15 +16,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late final BitmapDescriptor pinLocationIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(devicePixelRatio: 2.5),
+            'assets/images/icon_marker.png')
+        .then((onValue) {
+      pinLocationIcon = onValue;
+    });
+  }
+
   final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
+
+  Future<void> _onMapCreated(
+      GoogleMapController controller, LatLng pinPosition) async {
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
       _markers.clear();
+
       for (final office in googleOffices.offices) {
         final marker = Marker(
           markerId: MarkerId(office.name),
           position: LatLng(office.lat, office.lng),
+          icon: pinLocationIcon,
           infoWindow: InfoWindow(
             title: office.name,
             snippet: office.address,
@@ -30,22 +49,39 @@ class _MyAppState extends State<MyApp> {
         );
         _markers[office.name] = marker;
       }
+      // final marker = Marker(
+      //   markerId: MarkerId('office.name'),
+      //   position: pinPosition,
+      //   icon: pinLocationIcon,
+      //   infoWindow: InfoWindow(
+      //     title: 'office.name',
+      //     snippet: 'office.address',
+      //   ),
+      // );
+      // _markers.add(marker);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    LatLng pinPosition = const LatLng(37.3797536, -122.1017334);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Google Office Locations'),
-          backgroundColor: Colors.purpleAccent,
+          backgroundColor: Colors.purple,
         ),
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
+          myLocationEnabled: true,
+          onMapCreated: (_) {
+            _onMapCreated(_, pinPosition);
+          },
+          initialCameraPosition: CameraPosition(
+            //target: LatLng(0, 0),
+            target: pinPosition,
+            // bearing: 30,
             zoom: 2,
           ),
           markers: _markers.values.toSet(),
